c6bcd59fffc2d581e7ede786812b73c3d7973d41
Ikeda <brandon_ikeda@hotmail.com>
2017-12-03 18:49:51 -0800

    Added card, album list and album detail

--- App.js
+++ App.js
@@ -6,11 +6,15 @@ import {
   View
 } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <Header headerText={'Albums'}/>
+      <View>
+        <Header headerText={'Albums'}/>
+        <AlbumList />
+      </View>
     );
   }
 }
