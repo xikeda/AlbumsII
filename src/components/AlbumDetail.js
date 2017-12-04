cb98ea97bb1912670b045787c76cd6e1adba38de
Ikeda <brandon_ikeda@hotmail.com>
2017-12-03 20:11:11 -0800

    Styled List

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-import {Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+  const {title, artist, thumbnail_image} = album;
+
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <View style={styles.thumbnailContainerStyle }>
+          <Image
+          style={styles.thumbnailStyle}
+          source={{uri: thumbnail_image}}
+           />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
     </Card>
   );
+};
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
 };
 
 export default AlbumDetail;
